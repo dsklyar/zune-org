5054a92baca649619269b36e1b12e377f948fdee
Daniel <danielkSklyar@gmail.com>
2025-02-25 22:30:39 -0500

    Added path_provider to resolve db/folder structures which need to be created

--- zune_ui/lib/database/metadata.dart
+++ zune_ui/lib/database/metadata.dart
@@ -1,31 +1,43 @@
-import 'dart:io';
-
-import 'package:audio_metadata_reader/audio_metadata_reader.dart';
+part of database;
 
 class Metadata {
   final String? musicFolder;
   List<AudioMetadata> files = [];
 
   Metadata({
-    this.musicFolder = "music_dir",
+    this.musicFolder = "Zune Music",
   }) {
     initialize();
   }
 
-  void initialize() {
-    /// TODO: Need to figure out a wau to auto create folder if does not exist
-    /// NOTE: It seems that Directory.current is not the best way to get the path
-    ///       https://github.com/flutter/flutter/issues/135740
-    final root = Platform.environment['PWD']?.split('zune_ui').first ??
-        Directory.current.path.split('zune_ui').first;
-    var folder = Directory("$root$musicFolder")
+  Future<void> initialize() async {
+    /// NOTE: Creating the directory if it doesn't exist
+    ///       One can't simply create folders in OS, so using this for now:
+    ///       -> https://pub.dev/packages/path_provider
+    final appDir = await getApplicationDocumentsDirectory();
+    final directoryPath = "${appDir.path}/$musicFolder";
+
+    console.log("Directory path: $directoryPath", customTags: ["DIRECTORY"]);
+
+    final directory = Directory(directoryPath);
+    if (!directory.existsSync()) {
+      directory.createSync(recursive: true);
+
+      // There will be no files in this directory
+      // So returning
+      return;
+    }
+
+    var folder = Directory(directoryPath)
         .listSync(recursive: true)
         .whereType<File>()
-        .where((element) =>
-            element.path.contains("mp4") ||
-            element.path.contains("m4a") ||
-            element.path.contains("mp3") ||
-            element.path.contains("flac"))
+        .where(
+          (element) =>
+              element.path.contains("mp4") ||
+              element.path.contains("m4a") ||
+              element.path.contains("mp3") ||
+              element.path.contains("flac"),
+        )
         .toList();
 
     for (final file in folder) {
