c7673845b5f92c27ba8a2e631747f63bfa14b166
Daniel <danielkSklyar@gmail.com>
2025-03-03 18:54:58 -0500

    amended windowbar widget

--- zune_ui/lib/widgets/window_bar/widget.dart
+++ zune_ui/lib/widgets/window_bar/widget.dart
@@ -1,22 +1,29 @@
 part of window_bar_widget;
 
 class WindowBar extends StatelessWidget {
+  final GoRouter? router;
   const WindowBar({
     super.key,
+    this.router,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 128,
-      color: const Color.fromARGB(25, 255, 249, 231),
+      color: const Color.fromARGB(50, 255, 249, 231),
       child: WindowTitleBarBox(
         child: MoveWindow(
-            // child: GestureDetector(
-            //   onTap: () => context.go(ApplicationRoute.home.route),
-            //   child: const Icon(Icons.home),
-            // ),
-            ),
+            child: Row(
+          children: [
+            router != null
+                ? GestureDetector(
+                    onTap: () => router?.go(ApplicationRoute.home.route),
+                    child: const Icon(Icons.home),
+                  )
+                : const SizedBox.shrink(),
+          ],
+        )),
       ),
     );
   }
