859bff2244345fc3d0fb979bef39134ec0aa385d
Daniel <danielkSklyar@gmail.com>
2025-03-25 15:31:27 -0400

    adding search index configuration to track list view

--- zune_ui/lib/pages/music_page/track_list/index.dart
+++ zune_ui/lib/pages/music_page/track_list/index.dart
@@ -6,6 +6,8 @@ import 'package:flutter/widgets.dart';
 import 'package:zune_ui/database/index.dart';
 import 'package:zune_ui/pages/music_page/common/index.dart';
 import 'package:zune_ui/pages/overlays_page/index.dart';
+import 'package:zune_ui/pages/search_index_page/index.dart'
+    show SearchIndexConfig;
 import 'package:zune_ui/widgets/common/index.dart';
 
 /// NOTE: Scoping imports behind parent, so that console log is exposed from Music Page
@@ -13,5 +15,6 @@ import 'package:zune_ui/pages/music_page/index.dart' as parent;
 
 part "track_list.dart";
 part "track_list_tile.dart";
+part "constants.dart";
 
 final console = parent.console;
--- zune_ui/lib/pages/music_page/track_list/track_list.dart
+++ zune_ui/lib/pages/music_page/track_list/track_list.dart
@@ -1,6 +1,6 @@
 part of track_list_widget;
 
-const LIST_GAP = 20.0;
+typedef TrackGroupMap = LinkedHashMap<String, List<TrackSummary>>;
 
 class TrackList extends StatefulWidget {
   const TrackList({
@@ -12,21 +12,85 @@ class TrackList extends StatefulWidget {
 }
 
 class _TrackListState extends State<TrackList> {
-  List<TrackListTileGroup> _generateTrackGroups(
-    List<TrackSummary> tracks, {
-    ScrollController? scrollController,
-  }) =>
-      parent.generateItemGroups<TrackSummary, TrackListTileGroup>(
-        tracks,
-        (groupKey, item) => (groupKey: groupKey, track: item),
-        (e) => parent.generateItemGroupKey(e.track_name),
+  /// NOTE: Track List view allows "jumping" to a specific track via
+  ///       group keys rendered in the list.
+  ///
+  ///       This method is responsible for generating search index configuration map
+  ///       which represents a group key e.g. letter "a" mapped to a function that
+  ///       animated scroll controller to a location of the group key in the list.
+  ///
+  ///       Using pre-defined constant values to derive the group collection & key heights
+  ///       because the list view wrapper is dynamically built. This might not be the best
+  ///       solution to derive the search index configuration, perhaps a global solution
+  ///       might be faster.
+  void _generateSearchIndexConfiguration(
+      ScrollController? scrollController, TrackGroupMap trackGroupMap) {
+    // SearchIndexConfig is by default an empty map
+    if (scrollController == null) return;
+
+    double offset = 0.0;
+    SearchIndexConfig searchIndexConfiguration = {};
+
+    for (final entry in trackGroupMap.entries) {
+      // Since offset is a mutable closure, need to define a final value here
+      final currentOffset = offset;
+      searchIndexConfiguration.putIfAbsent(
+        entry.key,
+        () => () async => await scrollController.animateTo(
+              currentOffset,
+              duration: const Duration(milliseconds: 250),
+              curve: Curves.ease,
+            ),
       );
+
+      // Derive group collection & key heights to compute offsets needed for animation
+      final groupCollectionHeight = entry.value.fold(
+          offset, (acc, artist) => acc + TRACK_LIST_TILE_SIZE + TRACK_LIST_GAP);
+      const groupKeyHeight = TRACK_SEARCH_INDEX_TILE_SIZE + TRACK_LIST_GAP;
+
+      offset = groupCollectionHeight + groupKeyHeight;
+    }
+
+    OverlaysProvider.of(context)?.setSearchTileConfig(searchIndexConfiguration);
+  }
+
+  /// NOTE: This method is responsible for generating track groups which
+  ///       represent either a group key such as albums sorted under letter "a"
+  ///       OR the actual track entry. Both are rendered by TrackGroupTile.
+  ///
+  ///       There are two ways to generate group which specified in the utils:
+  ///       1. One shot method generateItemGroups which groups generateItemMap & generateItemListFromMap
+  ///       2. Use generateItemMap & generateItemListFromMap to fit search index configuration generation
+  List<TrackListTileGroup> _generateTrackGroups(
+    List<TrackSummary> artists, {
+    ScrollController? scrollController,
+  }) {
+    TrackGroupMap trackGroupMap = parent.generateItemMap(
+      artists,
+      (e) => parent.generateItemGroupKey(e.track_name),
+    );
+
+    _generateSearchIndexConfiguration(scrollController, trackGroupMap);
+
+    return parent.generateItemListFromMap(
+      trackGroupMap,
+      (groupKey, item) => (groupKey: groupKey, track: item),
+    );
+  }
+
+  @override
+  void deactivate() {
+    // Clear search tile config after the widget is deactivated
+    // Prevent case where config is present and used for incorrect widget
+    OverlaysProvider.of(context)?.setSearchTileConfig({});
+    super.deactivate();
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListWrapper<UnmodifiableListView<TrackSummary>, TrackSummary,
         TrackListTileGroup>(
-      listGap: LIST_GAP,
+      listGap: TRACK_LIST_GAP,
       selector: (state) => state.allTracks,
       itemBuilder: (context, trackGroup) =>
           TrackListTile(trackGroup: trackGroup),
