99e9b88f76a4ff5932fa2170041d59544f6b5d0b
Daniel <danielkSklyar@gmail.com>
2025-03-26 22:21:30 -0400

    updated the auto close logic in search index page

--- zune_ui/lib/pages/search_index_page/page.dart
+++ zune_ui/lib/pages/search_index_page/page.dart
@@ -23,8 +23,8 @@ class _SearchIndexPageState extends State<SearchIndexPage>
     with SingleTickerProviderStateMixin {
   late final Animation<double> _slideOutYOffsetAnimation;
   final Debouncer _autoCloseDebouncer = Debouncer(
-    duration: const Duration(seconds: 2),
-    debugName: "auto-close-effect",
+    duration: const Duration(seconds: 5),
+    debugName: "search-index-auto-close-effect",
     logger: console,
   );
 
@@ -33,8 +33,6 @@ class _SearchIndexPageState extends State<SearchIndexPage>
   @override
   void initState() {
     super.initState();
-
-    // console.log("render me");
 
     _slideOutYOffsetAnimation = TweenSequence<double>([
       TweenSequenceItem(
@@ -52,36 +50,44 @@ class _SearchIndexPageState extends State<SearchIndexPage>
     _triggerInitialAnimation();
   }
 
-  void _triggerInitialAnimation() {
-    // widget.parentController.animateTo(0.5).then(
-    //       (_) => _triggerDebouncer(),
-    //     );
-    widget.parentController.animateTo(0.5);
+  @override
+  void dispose() {
+    _autoCloseDebouncer.cancel();
+    super.dispose();
   }
 
-  void _animateAutoClose() {
-    // Start animation
-    widget.parentController.forward(from: 0.5).then((_) {
-      widget.closeOverlayHandler();
-      // setState(() {
-      //   // widget.parentController.reset();
-      //   // _panelIsInUse = false;
-      // });
-    });
-  }
+  void _triggerInitialAnimation() =>
+      widget.parentController.animateTo(0.5).then(
+            (_) => _animateAutoClose(),
+          );
 
-  void _triggerDebouncer() {
-    /// NOTE: Abstraction to trigger bounce animation delayed by the debouncer
-    _autoCloseDebouncer.call(() => _animateAutoClose());
-  }
-
-  void onSearchIndexTileTapHandler(String groupKey) {
-    if (widget.configuration.containsKey(groupKey)) {
-      final action = widget.configuration[groupKey]!;
-      _animateAutoClose();
-      action();
+  void _animateAutoClose({bool forceAnimate = false}) {
+    // AutoClose animation dispatch callback
+    dispatch() => widget.parentController
+        .forward(from: 0.5)
+        .then((_) => widget.closeOverlayHandler());
+    // Skip debounce
+    if (forceAnimate) {
+      dispatch();
+    } else {
+      _autoCloseDebouncer.call(dispatch);
     }
   }
+
+  /// NOTE: This method is responsible for executing the animate to
+  ///       callback if the groupKey is present in the configuration
+  ///       dictionary. If present, force execute the auto close animation
+  ///       and execute the callback.
+  void _onSearchIndexTileTapHandler(String groupKey) {
+    if (widget.configuration.containsKey(groupKey)) {
+      _animateAutoClose(forceAnimate: true);
+      widget.configuration[groupKey]!();
+    }
+  }
+
+  /// NOTE: Capture taps which do not result into scroll animation
+  ///       and debounce the auto close animation.
+  void _handleUnrelatedTap() => _animateAutoClose();
 
   @override
   Widget build(BuildContext context) {
@@ -92,6 +98,7 @@ class _SearchIndexPageState extends State<SearchIndexPage>
       builder: (context, child) => Transform.translate(
         offset: Offset(0, _slideOutYOffsetAnimation.value),
         child: GestureDetector(
+          onTap: _handleUnrelatedTap,
           child: Container(
             padding: const EdgeInsets.only(top: 8),
             color: Colors.black.withAlpha(250),
@@ -113,7 +120,8 @@ class _SearchIndexPageState extends State<SearchIndexPage>
                       index: indexKey[index],
                       isDisabled:
                           !widget.configuration.containsKey(indexKey[index]),
-                      onTap: () => onSearchIndexTileTapHandler(indexKey[index]),
+                      onTap: () =>
+                          _onSearchIndexTileTapHandler(indexKey[index]),
                     ),
             ),
           ),
